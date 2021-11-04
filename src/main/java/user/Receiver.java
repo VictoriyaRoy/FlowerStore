233672dbdd930b07a3506b185d7d0e1b3388f00b
VictoriyaRoy <victoriyaroi2003@gmail.com>
2021-11-04 23:43:27 +0200

    Add User id

--- src/main/java/user/Receiver.java
+++ src/main/java/user/Receiver.java
@@ -1,12 +1,18 @@
 package user;
 
 public class Receiver implements User{
+    private int id;
+
+    public Receiver() {
+        this.id = hashCode();
+    }
+
     @Override
     public void update(boolean status) {
         if(status){
-            System.out.println("User #id: Your order was sent");
+            System.out.printf("User #%d: Your order was sent", id);
         } else{
-            System.out.println("User #id: Your order wasn't sent");
+            System.out.printf("User #%d: Your order wasn't sent", id);
         }
     }
 }
--- src/main/java/user/Sender.java
+++ src/main/java/user/Sender.java
@@ -1,12 +1,18 @@
 package user;
 
 public class Sender implements User{
+    private int id;
+
+    public Sender() {
+        this.id = hashCode();
+    }
+
     @Override
     public void update(boolean status) {
         if(status){
-            System.out.println("User #id: Send order");
+            System.out.printf("User #%d: Send order\n", id);
         } else{
-            System.out.println("User #id: Can't send order");
+            System.out.printf("User #%d: Can't send order\n", id);
         }
     }
 }
